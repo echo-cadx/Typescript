7eaeeb59abb2c85f6f27804310ac98dfcecd5ac0
Elmer Toledo <mr.elmertoledo@gmail.com>
2024-08-21 13:03:04 +0800

    Add some function and different purpose

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log("hello typescipt");
+const getName = (name) => {
+    return name;
+};
+const AddTwoNumbers = (num1 = 0, num2 = 0) => {
+    return num1 + num2;
+};
+const ConcatStringAndAmount = (message, result) => {
+    return message + result;
+};
+const my_name = "Elmer Toledo";
+const sum_amount = AddTwoNumbers(5, 2);
+console.log(getName(my_name));
+console.log(sum_amount);
+console.log(ConcatStringAndAmount("The sum is ", sum_amount));
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,23 @@
-console.log("hello typescipt");
+
+
+const getName = (name : string)=>{
+
+    return name;
+}
+
+const AddTwoNumbers = (num1:number = 0,num2 :number = 0) => {
+
+    return num1 + num2;
+}
+
+const ConcatStringAndAmount = (message : string,result : number) =>{
+    return message + result;
+}
+
+const my_name : string = "Elmer Toledo"
+const sum_amount  = AddTwoNumbers(5,2);
+
+
+console.log(getName(my_name));
+console.log(sum_amount);
+console.log(ConcatStringAndAmount("The sum is ",sum_amount));
